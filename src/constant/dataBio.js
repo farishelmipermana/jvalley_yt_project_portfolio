664527a0ef191817c9234261a69382a99a80d3ba
Faris Helmi Permana <farissyarif23@gmail.com>
2022-12-19 12:06:53 +0700

    komit

--- src/constant/dataBio.js
+++ src/constant/dataBio.js
@@ -1,49 +1,47 @@
-
-export default  [
-    {
-        id : 1,
-        label : "Nama Lengkap",
-        value : "Selastio Fadli Rahman"
-    },
-    {
-        id : 2,
-        label : "Tempat Tanggal Lahir",
-        value : "Jakarta 13 Juli 1987"
-    },
-    {
-        id : 3,
-        label : "Nomer Telepon",
-        value : "081213655573",
-        link : "tel:+6281213655573",
-        icons : "phone"
-    },
-    {
-        id : 4,
-        label : "Email",
-        value : "fadliselalucoding@gmail.com",
-        link : "mailto:fadliselalucoding@gmail.com",
-        icons : "email"
-    },
-    {
-        id : 5,
-        label : "LinkedIn",
-        value : "@fadliselaz",
-        link : "https://linkedin.com/fadliselaz",
-        icons : "linkedin"
-    },
-    {
-        id : 6,
-        label : "Pendidikan Terakhir",
-        value : "Jvalley Bootcamp",
-        link : "https://jvalleybootcamp.com",
-        icons : "web"
-    },
-    {
-        id : 7,
-        label : "Alamat",
-        value : "Jl kh muhasan 1 no 79 Depok 1 Jawa Barat",
-        link : "https://map.google.com/depok",
-        icons : "map"
-    },
-    
-]
+export default [
+  {
+    id: 1,
+    label: "Nama Lengkap",
+    value: "Faris Helmi Permana",
+  },
+  {
+    id: 2,
+    label: "Tempat, Tanggal Lahir",
+    value: "Tasikmalaya, 23 Oktober 2001",
+  },
+  {
+    id: 3,
+    label: "Nomer Telepon",
+    value: "082116735990",
+    link: "tel:+6282116735990",
+    icons: "phone",
+  },
+  {
+    id: 4,
+    label: "Email",
+    value: "farissyarif23@gmail.com",
+    link: "mailto:farissyarif23@gmail.com",
+    icons: "email",
+  },
+  {
+    id: 5,
+    label: "LinkedIn",
+    value: "@farishelmi23",
+    link: "https://www.linkedin.com/in/farishelmi23/",
+    icons: "linkedin",
+  },
+  {
+    id: 6,
+    label: "Pendidikan Terakhir",
+    value: "Dicoding",
+    link: "https://www.dicoding.com/users/farishelmi/academies",
+    icons: "web",
+  },
+  {
+    id: 7,
+    label: "Alamat",
+    value: "Jln Raya Kalapasawit - Lakbok, 05/02 Kalapasawit, Lakbok, ciamis, Jawa Barat 46385",
+    link: "https://www.google.com/maps/place/Kalapasawit,+Lakbok,+Ciamis+Regency,+West+Java/@-7.3983192,108.6616147,15.25z/data=!4m5!3m4!1s0x2e65879239348867:0x423c1c48b5f63170!8m2!3d-7.389212!4d108.6383454",
+    icons: "map",
+  },
+];
